c0d8fd1aa80e26838cde544a38181dedcaa0edd9
preet006 <preetbhardwaj146@gmail.com>
2023-12-11 12:30:10 +0530

    change the layout of navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames'
 import {useSession} from 'next-auth/react'
-import { Box } from '@radix-ui/themes'
+import { Box, Container, Flex } from '@radix-ui/themes'
 
 const NavBar = () => {
    const currentPath =  usePathname();
@@ -15,7 +15,10 @@ const NavBar = () => {
         {label: 'Issues' , href:"/issues/list"}
     ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <nav className='border-b mb-5 px-5 py-3 '>
+        <Container>
+        <Flex justify='between'>
+            <Flex align={'center'} gap={'3'}>
         <Link href="/"><AiFillBug /></Link>
         <ul className='flex space-x-6 '>
           
@@ -29,11 +32,14 @@ const NavBar = () => {
             }
 
         </ul>
+    </Flex>
+    
         <Box>
             {status === 'authenticated' && <Link href='/api/auth/signout'>Log out</Link>}
             {status === 'unauthenticated' && <Link href='/api/auth/signin'>Log in</Link>}
         </Box>
-
+        </Flex>
+        </Container>
     </nav>
   )
 }
