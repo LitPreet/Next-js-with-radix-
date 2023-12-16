8a5fe23186e1fc3f0f878ad970de752b68c69df7
preet006 <preetbhardwaj146@gmail.com>
2023-12-16 15:08:30 +0530

    set up error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cde0f0a936025f4bce6de8e457e67ba7@o4506404703961088.ingest.sentry.io/4506404709859328",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
